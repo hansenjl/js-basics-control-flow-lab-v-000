c3e68dde201839e73ddd16a023f147e380decedb
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-01-10 20:18:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 function scuberGreetingForFeet(ride){
   if(ride<=400){
      "This one is on me!"
-  } else if (ride <= 3000 ){
+  } else if (ride > 2000 && ride < 3000 ){
     'I will gladly take your thirty bucks.'
-  } else {
+  } else if (ride > 3000) {
     'No can do'
   }
 }
