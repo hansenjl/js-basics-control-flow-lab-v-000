a41d8ceb8cf73e2190d8e31bd0e0be0778721dbf
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-01-10 20:19:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your code in this file!
 function scuberGreetingForFeet(ride){
-  if(ride<=400){
+  if(ride <= 400){
      "This one is on me!"
-  } else if (ride > 2000 && ride < 3000 ){
+  } else if (ride > 2000  ){
     'I will gladly take your thirty bucks.'
   } else if (ride > 3000) {
     'No can do'
